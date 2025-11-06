d441e38d514fdc498c7bd650fe549221ec3ef36e
Muhammad Hisyam Yassar Akbar <muhammadhisyamyassar@gmail.com>
2025-11-06 08:04:21 +0700

    Update users.php

--- users.php
+++ users.php
@@ -4,7 +4,8 @@ $users = [
     "Admin" => "pass@admiN1",
     "fajrul" => "pass@njulA2",
     "Gafur" => "pass@fur21",
-    "hisyam"  => "pass@hisyam24" // ganti dengan akunmu sendiri
+    "hisyam"  => "pass@hisyam24" 
 ];
 ?>
+
 
